6b38a8cf309c6df9a9cf0de4ff16d42bdda5b673
GTB3NW <GTB3NW@users.noreply.github.com>
2022-12-16 23:39:59 +0000

    fix switch

--- src/main.ts
+++ src/main.ts
@@ -169,8 +169,10 @@ async function run(): Promise<void> {
       switch (r.result) {
         case 'passed':
           icon = ':white_check_mark:'
+          break;
         case 'failed':
           icon = ':x:'
+          break;
       }
 
       rows.push([
